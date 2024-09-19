1a7e99b44b87b02abc8855333b4425183f5c0815
KillianMalon <killian.malon@epsi.fr>
2024-09-19 23:24:33 +0200

    add config and tests message broker

--- src/services/clients.js
+++ src/services/clients.js
@@ -1,21 +1,26 @@
-import ClientModel from '../models/client.js'
-import { NotFound } from '../globals/errors.js'
+import ClientModel from '../models/client.js';
+import { NotFound } from '../globals/errors.js';
+import { sendToQueue } from '../configurations/rabbitmq.js';
 
 export default class ClientService {
     constructor () {
         if (ClientService.instance instanceof ClientService) {
-            return ClientService.instance
+            return ClientService.instance;
         }
-        Object.freeze(this)
-        ClientService.instance = this
+
+        // Object.freeze(this);
+        ClientService.instance = this;
     }
 
     /**
      * Get all clients from db.
      */
     async getAll () {
-        const clients = await ClientModel.find()
-        return clients
+        const clients = await ClientModel.find();
+        // Envoyer un message à RabbitMQ après récupération de tous les clients
+        const message = JSON.stringify({ action: 'getAll', clients });
+        await sendToQueue('clientQueue', message);
+        return clients;
     }
 
     /**
@@ -24,22 +29,30 @@ export default class ClientService {
      * @param {ObjectId} clientId
      */
     async get ({ clientId }) {
-        const client = await ClientModel.findById(clientId)
+        const client = await ClientModel.findById(clientId);
         if (!client) {
-            throw new NotFound('Client introuvable.')
+            throw new NotFound('Client introuvable.');
         }
-        return client
+        // Envoyer un message à RabbitMQ après récupération du client
+        const message = JSON.stringify({ action: 'get', clientId, client });
+        await sendToQueue('clientQueue', message);
+        return client;
     }
 
     /**
-     * Create a custommer
+     * Create a customer
      * 
      * @param {Object} fields
      */
     async create ({ fields }) {
-        const client = new ClientModel(fields)
-        await client.save()
-        return client
+        const client = new ClientModel(fields);
+        await client.save();
+        
+        // Envoyer un message à RabbitMQ après la création du client
+        const message = JSON.stringify({ action: 'create', clientId: client._id, fields });
+        await sendToQueue('clientQueue', message);
+
+        return client;
     }
 
     /**
@@ -49,11 +62,16 @@ export default class ClientService {
      * @param {Object} fields
      */
     async update ({ clientId, fields }) {
-        const client = await ClientModel.findByIdAndUpdate(clientId, fields, { new: true })
+        const client = await ClientModel.findByIdAndUpdate(clientId, fields, { new: true });
         if (!client) {
-            throw new NotFound('Client introuvable.')
+            throw new NotFound('Client introuvable.');
         }
-        return client
+
+        // Envoyer un message à RabbitMQ après la mise à jour du client
+        const message = JSON.stringify({ action: 'update', clientId, fields });
+        await sendToQueue('clientQueue', message);
+
+        return client;
     }
 
     /**
@@ -62,9 +80,13 @@ export default class ClientService {
      * @param {ObjectId} clientId
      */
     async remove ({ clientId }) {
-        const client = await ClientModel.findByIdAndDelete(clientId)
+        const client = await ClientModel.findByIdAndDelete(clientId);
         if (!client) {
-            throw new NotFound('Client introuvable.')
+            throw new NotFound('Client introuvable.');
         }
+
+        // Envoyer un message à RabbitMQ après la suppression du client
+        const message = JSON.stringify({ action: 'delete', clientId });
+        await sendToQueue('clientQueue', message);
     }
 }
